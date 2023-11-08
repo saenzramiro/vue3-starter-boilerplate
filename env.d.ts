5ebc51facf000ad2db6dc70ef725aa432b334d98
Ramiro Saenz <saenzramiro@gmail.com>
2023-11-08 18:19:16 -0300

    Linted

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string;
-  // more env variables...
+	readonly VITE_APP_TITLE: string;
+	// more env variables...
 }
